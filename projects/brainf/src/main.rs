bfd8fe54d3addf2900c1797c9588f76fa1e70066
daws.eth <3408480+dawsbot@users.noreply.github.com>
2022-04-09 15:20:42 -0700

    complete "Hello World!"

--- projects/brainf/src/main.rs
+++ projects/brainf/src/main.rs
@@ -1,40 +1,77 @@
 use std::io;
 fn main() {
-    let program = ",.";
-    // let program = ",.+>>+......";
-    let mut tape = [0u8; 100];
-    let mut pointer: usize = 0;
+    // let program = ",.";
+    let program = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]
+    <.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.>++++++++++.";
 
-    for c in program.chars() {
-        if c == '+' {
-            tape[pointer] = tape[pointer] + 1;
+    // let program = "++++[>+++++<-]>[<+++++>-]+<+[
+    //     >[>+>+<<-]++>>[<<+>>-]>>>[-]++>[-]+
+    //     >>>+[[-]++++++>>>]<<<[[<++++++++<++>>-]+<.<[>----<-]<]
+    //     <<[>>>>>[>>>[-]+++++++++<[>-<-]+++++++++>[-[<->-]+[<<<]]<[>+<-]>]<<-]<<-
+    // ]";
+    // let program = ",.+>>+......";
+    let mut tape = [0u8; 30000];
+
+    // Where the processor is at in the program
+    let mut program_counter = 0;
+    // Where the current cell is in the tape
+    let mut address_pointer = 0;
+
+    let program_len = program.len();
+    while program_counter < program_len {
+        let current_char = program.chars().nth(program_counter).unwrap();
+
+        // loops
+        /*
+        Jump to closing bracket if the current cell is zero
+         */
+        if current_char == '[' {
+            if tape[address_pointer] == 0 {
+                // find index of next ]
+                while program.chars().nth(program_counter).unwrap() != ']' {
+                    program_counter = program_counter + 1;
+                }
+            }
         }
-        if c == '-' {
-            tape[pointer] = tape[pointer] - 1;
+        if current_char == ']' {
+            if tape[address_pointer] != 0 {
+                // find index of next ]
+                while program.chars().nth(program_counter).unwrap() != '[' {
+                    program_counter = program_counter - 1;
+                }
+            }
         }
-        if c == '>' {
-            pointer = pointer + 1;
+
+        if current_char == '+' {
+            tape[address_pointer] = tape[address_pointer] + 1;
         }
-        if c == '<' {
-            pointer = pointer - 1;
+        if current_char == '-' {
+            tape[address_pointer] = tape[address_pointer] - 1;
         }
-        if c == '.' {
-            println!("{}", tape[pointer] as char)
+        if current_char == '>' {
+            address_pointer = address_pointer + 1;
         }
-        if c == ',' {
+        if current_char == '<' {
+            address_pointer = address_pointer - 1;
+        }
+        // print char at current point in the tape
+        if current_char == '.' {
+            print!("{}", tape[address_pointer] as char)
+        }
+        // take input from user and store it in the tape
+        if current_char == ',' {
             let mut user_input = String::new();
             println!("Please input a number");
             io::stdin()
                 .read_line(&mut user_input)
                 .expect("Failed to read line");
-
             let user_input = match user_input.trim().parse() {
                 Ok(num) => num,
                 Err(_) => continue,
             };
-
-            tape[pointer] = user_input;
+            tape[address_pointer] = user_input;
         }
+        program_counter += 1;
     }
 
     // println!("Full tape: {:?}", tape);
