f1975238d31c1bf3b49e0732c80e40138a047b52
Dawson Botsford <github.81841d@tryninja.io>
2022-04-17 12:51:43 -0700

    🧼 Brainf optims (#2)
    * improve readme
    
    * allow integer wrapping
    
    * fix tape overflwo
    
    * lexer parser
    
    * parser
    
    * fix most edge cases
    
    * fix basic tests
    
    * optims
    
    * Store memory size in const
    
    * remove ast faking
    
    * mandelbrot working
    
    * optims
    
    * remove unused comments
    
    * cleanup

--- projects/brainf/src/main.rs
+++ projects/brainf/src/main.rs
@@ -1,95 +1,206 @@
 use std::env;
 use std::fs;
 use std::io;
-fn main() {
-    let args: Vec<String> = env::args().collect();
+use std::num::Wrapping;
 
-    let filename = &args[1];
+#[derive(Debug, Clone, PartialEq)]
+enum Tokens {
+    Decrement,
+    Increment,
+    ShiftLeft,
+    ShiftRight,
+    Print,
+    TakeInput,
+    StartLoop,
+    EndLoop,
+    // NewLine,
+}
 
-    let program = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    // println!("The program: {}", program);
-    let mut tape = [0u8; 30000];
+// step 1 - removes non-necessary characters
+fn lex(program: String) -> Vec<Tokens> {
+    let mut lexed: Vec<Tokens> = Vec::new();
+    for c in program.chars() {
+        match c {
+            '+' => lexed.push(Tokens::Increment),
+            '-' => lexed.push(Tokens::Decrement),
+            '<' => lexed.push(Tokens::ShiftLeft),
+            '>' => lexed.push(Tokens::ShiftRight),
+            '.' => lexed.push(Tokens::Print),
+            ',' => lexed.push(Tokens::TakeInput),
+            '[' => lexed.push(Tokens::StartLoop),
+            ']' => lexed.push(Tokens::EndLoop),
+            // '\n' => lexed.push(Tokens::NewLine),
+            _ => (),
+        }
+    }
+    return lexed;
+}
 
-    // Where the processor is at in the program
-    let mut program_counter = 0;
-    // Where the current cell is in the tape
-    let mut address_pointer = 0;
+#[derive(Debug, Clone)]
+struct ASTEntry {
+    token: Tokens,
+    consecutive_count: usize,
+    // only opening and closing brackets get this
+    matching_bracket: Option<usize>,
+}
 
-    let program_len = program.len();
+// step 2
+// recursive-descent parser was turning out difficult, so made this an optimized interpreter instead
+fn assemble_tokens(lexed: Vec<Tokens>) -> Vec<ASTEntry> {
+    let mut tokens: Vec<ASTEntry> = vec![];
+    let mut i = 0;
 
-    // stores the start index of each loop we're inside
-    let mut loop_start_indices = vec![];
+    // to know when we are inside a loop
+    let mut opening_loop_indices: Vec<usize> = vec![];
 
-    let program_chars: Vec<char> = program.chars().collect();
+    while i < lexed.len() {
+        let token = &lexed[i];
+        let mut ast_entry = ASTEntry {
+            token: token.clone(),
+            matching_bracket: None,
+            consecutive_count: 1,
+        };
+        match token {
+            Tokens::StartLoop => {
+                tokens.push(ast_entry);
+                opening_loop_indices.push(tokens.len() - 1);
+            }
+            Tokens::EndLoop => {
+                let matching_bracket = opening_loop_indices.pop().unwrap();
+                tokens[matching_bracket].matching_bracket = Some(tokens.len() - 1);
+                ast_entry.matching_bracket = Some(matching_bracket);
+                tokens.push(ast_entry);
+            }
+            _ => {
+                let genesis_token = token;
+                while i < lexed.len() - 1 && lexed[i + 1] == *genesis_token {
+                    ast_entry.consecutive_count += 1;
+                    i += 1;
+                }
+                tokens.push(ast_entry);
+            }
+        }
+        i += 1;
+    }
+    // dbg!(&tokens);
+    return tokens;
+}
 
-    while program_counter < program_len {
-        let cell_value = tape[address_pointer];
+const MEMORY_SIZE: usize = 30_000;
+fn run(program: String) -> [Wrapping<u8>; MEMORY_SIZE] {
+    let lexed = lex(program);
+    let tokens = assemble_tokens(lexed);
 
-        match program_chars[program_counter] {
-            /*
-            Jump to closing bracket if the current cell is zero
-             */
-            '[' => {
-                if cell_value == 0 {
-                    let mut inside_loop_count = 0;
-                    // move to matching ]
-                    loop {
-                        program_counter += 1;
-                        let current_char = program_chars[program_counter];
-                        if current_char == '[' {
-                            inside_loop_count += 1;
-                        } else if current_char == ']' {
-                            if inside_loop_count == 0 {
-                                break;
-                            }
-                            inside_loop_count -= 1;
-                        }
-                    }
-                } else {
-                    loop_start_indices.push(program_counter);
+    let mut tape = [Wrapping(0u8); MEMORY_SIZE];
+    let mut tape_index = 0;
+
+    let mut tokens_index = 0;
+    let tokens_len = tokens.len();
+
+    // error address_pointer needs to be nested-aware to access value from the tape properly
+    while tokens_index < tokens_len {
+        let cell_value = tape[tape_index];
+        let consecutive_count = tokens[tokens_index].consecutive_count;
+        match tokens[tokens_index].token {
+            Tokens::StartLoop => {
+                if cell_value == Wrapping(0) {
+                    tokens_index = tokens[tokens_index].matching_bracket.unwrap();
                 }
             }
-            ']' => {
-                if cell_value == 0 {
-                    loop_start_indices.pop();
-                } else {
-                    // pop back to start of loop
-                    program_counter = loop_start_indices.pop().unwrap() - 1;
+            Tokens::EndLoop => {
+                // println!("end loop, cell_value {}", cell_value);
+                if cell_value != Wrapping(0) {
+                    tokens_index = tokens[tokens_index].matching_bracket.unwrap() - 1;
                 }
             }
-
-            '+' => {
-                tape[address_pointer] = cell_value + 1;
+            Tokens::Increment => {
+                tape[tape_index] = cell_value + Wrapping(consecutive_count as u8);
             }
-            '-' => {
-                tape[address_pointer] = cell_value - 1;
+            Tokens::Decrement => {
+                tape[tape_index] = cell_value - Wrapping(consecutive_count as u8);
             }
-            '>' => {
-                address_pointer = address_pointer + 1;
+            Tokens::ShiftRight => {
+                tape_index = (tape_index + consecutive_count) % (MEMORY_SIZE - 1);
             }
-            '<' => {
-                address_pointer = address_pointer - 1;
+            Tokens::ShiftLeft => {
+                tape_index = ((MEMORY_SIZE - consecutive_count) + tape_index) % MEMORY_SIZE;
             }
             // print char at current point in the tape
-            '.' => {
-                print!("{}", cell_value as char)
+            Tokens::Print => {
+                for _ in 0..consecutive_count {
+                    print!("{}", (cell_value.0 as char))
+                }
             }
             // take input from user and store it in the tape
-            ',' => {
+            Tokens::TakeInput => {
                 let mut user_input = String::new();
                 println!("Please input a number");
                 io::stdin()
                     .read_line(&mut user_input)
                     .expect("Failed to read line");
                 let user_input = match user_input.trim().parse() {
-                    Ok(num) => num,
+                    Ok(num) => Wrapping(num),
                     Err(_) => continue,
                 };
-                tape[address_pointer] = user_input;
+                tape[tape_index] = user_input;
             }
-            _ => (),
         }
-        program_counter += 1;
+        tokens_index += 1;
     }
-    // println!("Full tape: {:?}", tape);
+    // dbg!(&tape);
+    return tape;
+}
+
+fn read_file() -> String {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let program = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    program
+}
+
+fn main() {
+    let program = read_file();
+    run(program);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_plus() {
+        let program = "+";
+        let res = run(String::from(program));
+        assert_eq!(res[0], Wrapping(1));
+        assert_eq!(res[1], Wrapping(0));
+    }
+
+    #[test]
+    fn test_overflow_minus() {
+        let program = "<-";
+        let res = run(String::from(program));
+        assert_eq!(res[MEMORY_SIZE - 1], Wrapping(255));
+        assert_eq!(res[0], Wrapping(0));
+    }
+    #[test]
+    fn test_small_loop() {
+        let program = "+[-]";
+        let res = run(String::from(program));
+        assert_eq!(res[0], Wrapping(0));
+    }
+    #[test]
+    fn test_comment_loop() {
+        let program = "+>[+should skip this+]";
+        let res = run(String::from(program));
+        assert_eq!(res[0], Wrapping(1));
+        assert_eq!(res[1], Wrapping(0));
+    }
+    #[test]
+    fn test_nested_loop() {
+        let program = "+>+[+>+[-<]]";
+        let res = run(String::from(program));
+        assert_eq!(res[0], Wrapping(0));
+        assert_eq!(res[1], Wrapping(1));
+        assert_eq!(res[2], Wrapping(0));
+    }
 }
